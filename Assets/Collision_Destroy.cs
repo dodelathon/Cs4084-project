faf6ae5d05fb9d86ad8d52b65be02ecee2dd3172
dodelathon <36057018+dodelathon@users.noreply.github.com>
2018-03-24 20:08:56 +0000

    death...able
    player and zombie can die

---
+++ Assets/Collision_Destroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Destroy : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "FastZombie")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
