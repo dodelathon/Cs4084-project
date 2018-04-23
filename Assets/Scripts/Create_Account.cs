6726a823e7e7d4a95bb845a4125fdb8ac960e10a
dairelavelle1 <16192249@studentmail.ul.ie>
2018-04-23 16:43:09 +0100

    SOHLogin added

---
+++ Assets/Scripts/Create_Account.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Auth;
+
+public class Create_Account : MonoBehaviour {
+	Firebase.Auth.FirebaseAuth auth;
+	public InputField DisplayNameIn;
+	
+	public void submitButtonPressed() {
+		Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+		if (user != null) {
+  		Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile {
+  			DisplayName=DisplayNameIn.text,
+  		};
+  		user.UpdateUserProfileAsync(profile).ContinueWith(task => {
+		    if (task.IsCanceled) {
+		      Debug.LogError("UpdateUserProfileAsync was canceled.");
+		      return;
+		    }
+		    if (task.IsFaulted) {
+		      Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+		      return;
+		    }
+		    SceneManager.LoadSceneAsync("Menu");
+		    Debug.Log("User profile updated successfully.");
+			});
+  		}
+	}
+}
--- Assets/Scripts/SOHLogin.cs
+++ Assets/Scripts/SOHLogin.cs
@@ -7,23 +7,20 @@ public class SOHLogin : MonoBehaviour {
 	public InputField EmailAddress, Password;
 
 	public void LoginButtonPressed() {
-		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
-			ContinueWith((obj)=> {
-				SceneManager.LoadSceneAsync("Main");
-			});
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).ContinueWith((obj)=> {
+			SceneManager.LoadSceneAsync("Menu");
+		});
 	}
 
 	public void GuestButtonPressed() {
-		FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().
-			ContinueWith((obj)=> {
-				SceneManager.LoadSceneAsync("Main");
-			});
+		FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().ContinueWith((obj)=> {
+			SceneManager.LoadSceneAsync("Menu");
+		});
 	}
 
 	public void CreateNewUserButtonPressed() {
-		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
-			ContinueWith((obj)=> {
-				SceneManager.LoadSceneAsync("Main");
-			});
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(EmailAddress.text, Password.text).ContinueWith((obj)=> {
+			SceneManager.LoadSceneAsync("create_account");
+		});
 	}
 }
