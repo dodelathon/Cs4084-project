25e0bf890a2cc04499effee8e8430602fac488c8
dairelavelle1 <16192249@studentmail.ul.ie>
2018-04-23 15:00:32 +0100

    login page and firebase

---
+++ Assets/Scripts/SOHLogin.cs
@@ -0,0 +1,29 @@
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SOHLogin : MonoBehaviour {
+	public InputField EmailAddress, Password;
+
+	public void LoginButtonPressed() {
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
+			ContinueWith((obj)=> {
+				SceneManager.LoadSceneAsync("Main");
+			});
+	}
+
+	public void GuestButtonPressed() {
+		FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().
+			ContinueWith((obj)=> {
+				SceneManager.LoadSceneAsync("Main");
+			});
+	}
+
+	public void CreateNewUserButtonPressed() {
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
+			ContinueWith((obj)=> {
+				SceneManager.LoadSceneAsync("Main");
+			});
+	}
+}
