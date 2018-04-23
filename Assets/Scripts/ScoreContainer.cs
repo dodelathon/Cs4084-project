7c7850ccef8b2ef65f6ed98399a4453244183491
dodelathon <36057018+dodelathon@users.noreply.github.com>
2018-04-23 16:21:13 +0100

    firebase

---
+++ Assets/Scripts/ScoreContainer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreContainer : MonoBehaviour
+{
+    public static ScoreContainer container;
+
+    List<int> scores = new List<int>();
+
+    // Use this for initialization
+    void Awake ()
+    {
+        if(container == null)
+        {
+            container = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(container != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
